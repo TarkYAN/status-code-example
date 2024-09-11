854e2a3c3dcc4c47789b624da7b2058a32499cdf
Austin Willoughby <arwigm@rit.edu>
2024-09-11 11:23:33 -0400

    Corrected copy/paste error

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -5,7 +5,7 @@ const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const getIndex = (request, response) => {
   response.writeHead(200, { 
     'Content-Type': 'text/html',
-    'Content-Length': Buffer.byteLength(content, 'utf8'),
+    'Content-Length': Buffer.byteLength(index, 'utf8'),
  });
   response.write(index);
   response.end();
